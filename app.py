f9311a5c7da642ba57eccd600e0313b333a80e43
YuriiKharabara <yurii.kharabara@ucu.edu.ua>
2023-05-16 23:48:47 +0300

    front without css is here

---
+++ app.py
@@ -0,0 +1,35 @@
+# app.py
+from flask import Flask, request, render_template, redirect
+import os
+import zipfile
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/', methods = ['GET', 'POST'])
+def upload_image_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = os.path.join('static', f.filename)
+      f.save(filename)
+      print(filename)
+
+      if zipfile.is_zipfile(filename):
+         with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall('static')
+         os.remove(filename)
+
+      # Call your function here and get the result
+      # result = your_function(filename)
+    #   print(filename)
+      result = [1, 2, 3, 4, 5]
+      
+      return render_template('result.html', result = result)
+   else:
+      return redirect('/')
+
+if __name__ == '__main__':
+   app.run(debug = True)
